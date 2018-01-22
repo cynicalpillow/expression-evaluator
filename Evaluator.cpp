dee3eb1c5f80f7ef8aca6afb923cfbc01e966bb5
Rui Li <cynicalrui@gmail.com>
2018-01-22 18:14:21 -0500

    Fixed few issues with unary operators and implicit multiplication. There may be some bugs leftover, but tested ones are all fixed. Additionally, this fixed the issue of 6/2(2+1)=1, when it should have been 9

--- Evaluator.cpp
+++ Evaluator.cpp
@@ -5,7 +5,7 @@ using namespace std;
  * equation solver (?)
  */
 
-const int OPERATORS = 17;
+const int OPERATORS = 18;
 int EVALUATION_ERROR = 0;
 
 //Tokenizing
@@ -13,7 +13,7 @@ string original_expression;
 vector<string> tokens;
 
 //Operators and precedence values
-string operators[OPERATORS] = {"+", "-", "*", "/", "^", "sin", "cos", "tan", "ln", "log", "log10", "log2", "exp", "exp2", "sqrt", "-u", "!"};
+string operators[OPERATORS] = {"+", "-", "*", "i*", "/", "^", "sin", "cos", "tan", "ln", "log", "log10", "log2", "exp", "exp2", "sqrt", "-u", "!"};
 map<string, int> precedences;
 map<string, int> associativity;
 
@@ -41,6 +41,7 @@ void tokenize(){
 	int right = 0;
 	int left = 0;
 	bool update = false;
+	bool digit = false;
 	for(int i = 0; i < n; i++){
 		if(update){
 			left = i;
@@ -48,31 +49,49 @@ void tokenize(){
 			update = false;
 		}
 		if(isdigit(original_expression[i]) 
-			|| original_expression[i] == '.' 
-			|| (original_expression[i] >= 'a' && original_expression[i] <= 'z')){
-			right++;
+			|| original_expression[i] == '.' ){
+			if(!digit && i > 0 
+				&& original_expression.substr(left, right-left) != "log"
+				&& original_expression.substr(left, right-left) != "exp"){
+				if(left != right)tokens.push_back(original_expression.substr(left, right-left)); //Don't add an empty string, otherwise add the function
+				left = i;
+				right = i+1;
+			} else {
+				right++;
+			}
+			digit = true;
+		} else if((original_expression[i] >= 'a' && original_expression[i] <= 'z')){
+			if(digit){
+				if(left != right)tokens.push_back(original_expression.substr(left, right-left)); //Don't add an empty string, otherwise add the number
+				left = i;
+				right = i+1;
+			} else {
+				right++;
+			}
+			digit = false;
 		} else {
-			if(left != right)tokens.push_back(original_expression.substr(left, right-left)); //Don't add an empty string, otherwise add the number
+			if(left != right)tokens.push_back(original_expression.substr(left, right-left)); //Don't add an empty string, otherwise add the number or function
 			tokens.push_back(original_expression.substr(i, 1)); //Add the operator
 			update = true; //move up left and right pointer to next position after operator
+			digit = false;
 		}
 	}
-	if(left != right && !update)tokens.push_back(original_expression.substr(left, right-left)); //Don't add an empty string, otherwise add the number
+	if(left != right && !update)tokens.push_back(original_expression.substr(left, right-left)); //Don't add an empty string, otherwise add the number or function
 }
 
 void initialize_operator_precedence(){
 	for(int i = 0; i < OPERATORS; i++){
 		if(i < 2)precedences[operators[i]] = 2;
-		else if(i < 4)precedences[operators[i]] = 3;
 		else if(i < 5)precedences[operators[i]] = 4;
-		else if(i == 15)precedences[operators[i]] = 4;
-		else precedences[operators[i]] = 5;
+		else if(i < 6)precedences[operators[i]] = 5;
+		else if(i == 16)precedences[operators[i]] = 5;
+		else precedences[operators[i]] = 6;
 	}
 }
 
 void initialize_operator_associativity(){
 	for(int i = 0; i < OPERATORS; i++){
-		if(i != 4 && i != 15)associativity[operators[i]] = 1;
+		if(i != 5 && i != 16)associativity[operators[i]] = 1;
 		else associativity[operators[i]] = 0;
 	}
 }
@@ -84,62 +103,12 @@ bool is_operator(string x){
 	return false;
 }
 
-int shunting_yard(){
-	bool beginning = true;
-	bool prev_was_operator = false;
-	bool unary = false;
-	for(string s : tokens){
-		if(s == "-" && (beginning || prev_was_operator || unary)){
-			unary = true;
-			operator_stack.push("-u");
-		} else if(is_operator(s)){
-			while(operator_stack.size() > 0 && ((precedences[operator_stack.top()] > precedences[s]) 
-				|| (precedences[operator_stack.top()] == precedences[s] && associativity[operator_stack.top()])) 
-				&& operator_stack.top() != "("){
-				output_queue.push_back(operator_stack.top());
-				operator_stack.pop();
-			}
-			operator_stack.push(s);
-			if(s != "!")prev_was_operator = true;
-		} else if(s == "("){
-			if(!prev_was_operator && !unary)operator_stack.push("*");
-			operator_stack.push(s);
-			prev_was_operator = true;
-		} else if(s == ")"){
-			while(operator_stack.size() > 0 && operator_stack.top() != "("){
-				output_queue.push_back(operator_stack.top());
-				operator_stack.pop();
-			}
-			if(operator_stack.size() < 1){
-				cout << "Error with brackets" << endl;
-				return -1;
-			}
-			operator_stack.pop();
-			prev_was_operator = false;
-		} else {
-			output_queue.push_back(s);
-			prev_was_operator = false;
-			unary = false;
-		}
-		beginning = false;
-	}
-	while(operator_stack.size() > 0){
-		if(operator_stack.top() == "("){
-			cout << "Error with brackets" << endl;
-			return -1;
-		}
-		output_queue.push_back(operator_stack.top());
-		operator_stack.pop();
-	}
-	return 0;
-}
-
 double get_value(double operand1, double operand2, string op){
 	if(op == "+"){
 		return operand1+operand2;
 	} else if(op == "-"){
 		return operand1-operand2;
-	} else if(op == "*"){
+	} else if(op == "*" || op == "i*"){
 		return operand1*operand2;
 	} else if(op == "^"){
 		return pow(operand1, operand2);
@@ -180,23 +149,102 @@ double get_value(double operand1, string op){
 
 bool is_function_unary_operator(string s){
 	for(int i = 0; i < OPERATORS; i++)
-		if(operators[i] == s && i > 4)return true;
+		if(operators[i] == s && i > 5)return true;
 	return false;
 }
 
+int shunting_yard(){
+	bool beginning = true;
+	bool prev_was_operator = false;
+	bool unary = false;
+	for(string s : tokens){
+		if(s == "-" && (beginning || prev_was_operator || unary)){ //Unary minus
+			unary = true;
+			operator_stack.push("-u");
+		} else if(is_operator(s)){
+			//implicit multiplication
+			if(is_function_unary_operator(s) && !prev_was_operator && s != "!" && !beginning){
+				while(operator_stack.size() > 0 && ((precedences[operator_stack.top()] > precedences["i*"]) 
+					|| (precedences[operator_stack.top()] == precedences["i*"] && associativity[operator_stack.top()])) 
+					&& operator_stack.top() != "("){
+					output_queue.push_back(operator_stack.top());
+					operator_stack.pop();
+				}
+				operator_stack.push("i*");
+			}
+			//Regular operations and functions
+			while(operator_stack.size() > 0 && ((precedences[operator_stack.top()] > precedences[s]) 
+				|| (precedences[operator_stack.top()] == precedences[s] && associativity[operator_stack.top()])) 
+				&& operator_stack.top() != "("){
+				output_queue.push_back(operator_stack.top());
+				operator_stack.pop();
+			}
+			operator_stack.push(s);
+			//Check if factorial
+			if(s == "!")unary = true;
+			if(s != "!")prev_was_operator = true;
+			unary = false;
+		} else if(s == "("){
+			//implicit multiplication
+			if(!prev_was_operator && !unary && !beginning){
+				while(operator_stack.size() > 0 && ((precedences[operator_stack.top()] > precedences["i*"]) 
+					|| (precedences[operator_stack.top()] == precedences["i*"] && associativity[operator_stack.top()])) 
+					&& operator_stack.top() != "("){
+					output_queue.push_back(operator_stack.top());
+					operator_stack.pop();
+				}
+				operator_stack.push("i*");
+			}
+			operator_stack.push(s);
+			prev_was_operator = true; //considered an operator for cases with unary operators
+			unary = false;
+		} else if(s == ")"){
+			while(operator_stack.size() > 0 && operator_stack.top() != "("){
+				output_queue.push_back(operator_stack.top());
+				operator_stack.pop();
+			}
+			if(operator_stack.size() < 1){
+				cout << "Error with brackets" << endl;
+				return -1;
+			}
+			operator_stack.pop();
+			prev_was_operator = false; //Not an operator, as it's essentially returning an operand
+			unary = false;
+		} else {
+			output_queue.push_back(s);
+			prev_was_operator = false;
+			unary = false;
+		}
+		beginning = false;
+	}
+	while(operator_stack.size() > 0){
+		if(operator_stack.top() == "("){
+			cout << "Error with brackets" << endl;
+			return -1;
+		}
+		output_queue.push_back(operator_stack.top());
+		operator_stack.pop();
+	}
+	return 0;
+}
+
 double evaluate(){
+	//Shunting yard algorithm to get expression into post-fix notation
+	int error = shunting_yard();
+	if(error == -1)return nan("");
+	//Evaluate the post-fix expression
 	for(string s : output_queue){
 		if(is_function_unary_operator(s)){
 			if(operand_stack.size() < 1){
 				EVALUATION_ERROR = 1;
-				return 0;
+				return nan("");
 			}
 			double operand_1 = atof(operand_stack.top().c_str()); operand_stack.pop();
 			operand_stack.push(to_string(get_value(operand_1, s)));
 		}else if(is_operator(s)){
 			if(operand_stack.size() < 2){
 				EVALUATION_ERROR = 1;
-				return 0;
+				return nan("");
 			}
 			double operand_2 = atof(operand_stack.top().c_str()); operand_stack.pop();
 			double operand_1 = atof(operand_stack.top().c_str()); operand_stack.pop();
@@ -206,6 +254,7 @@ double evaluate(){
 		}
 	}
 	double ret = atof(operand_stack.top().c_str()); operand_stack.pop();
+	if(ret == -0)return 0;
 	return ret;
 }
 
@@ -216,14 +265,8 @@ int main(){
 	tokenize();
 	initialize_operator_precedence();
 	initialize_operator_associativity();
-
-	//Shunting yard algorithm to get expression into post-fix notation
-	int error = shunting_yard();
-	if(error == -1)return 0;
-
-	//Evaluate the post-fix expression
 	double ans = evaluate();
-	cout << original_expression << "=" << endl;
+	cout << original_expression << " = ";
 	if(EVALUATION_ERROR)cout << "Error in evaluating" << endl;
 	else cout << ans << endl;
 	return 0;
